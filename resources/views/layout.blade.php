10d0c15e4bdac8945ee7618d56a64dca641be906
javahomework <dinhquanghuy1802@gmail.com>
2026-04-18 07:48:38 +0700

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Quản Lý Sinh Viên</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container mt-4">
+
+<h2 class="text-center mb-4">Quản Lý Sinh Viên</h2>
+
+@yield('content')
+
+</div>
+
+</body>
+</html>
